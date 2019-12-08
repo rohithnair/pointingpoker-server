dfe9f00e4fbbad4d49eaa4f24ca82c4655f06d0c
rohithnair <rrohithr@gmail.com>
2019-12-08 18:58:23 +0000

    basic socket communication established with express server.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        console.log(data);
+    })});
+
 server.listen(process.env.PORT || 8999, () => {
-    
+
+    console.log("Server is running");
 });
